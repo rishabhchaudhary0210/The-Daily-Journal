8de2b15275a455cfe8b341314e82314502917ad0
Rishabh <chaudharyrishabh0210@gmail.com>
2023-06-08 15:05:57 +0530

    fixing style errors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 const port = process.env.PORT || 80;
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname,'/views'));
 app.set("view engine","ejs");
 
